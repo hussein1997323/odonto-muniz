1304306d20cc40fd189fac804800221bfec44d89
Joyce Cerqueira da Silva <joyce.cerqueiras@gmail.com>
2023-03-15 12:25:51 -0300

    Inclusão de novas funcionalidades e nova página "Cadastro"
    Co-Authored-By: hussein1997323 <127331707+hussein1997323@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert(Olá)
+const cadastro = document.getElementById("btnCadastro");
+
+cadastro.addEventListener("click", fnNewPage)
+
+function fnNewPage(btnCadastro){
+   const win = window.open("./cadastro/formCadastro.html")
+}
+
+const prontuario = document.getElementById("btnProntuario");
+
+prontuario.addEventListener("click", fnNewPageTwo)
+
+function fnNewPageTwo(){
+    alert ("ESTÁ FUNCIONANDO 2")
+    return
+
+}
+
+const agenda = document.getElementById("btnAgenda");
+
+agenda.addEventListener("click", fnNewPageThree)
+
+function fnNewPageThree(){
+    alert ("ESTÁ FUNCIONANDO 3")
+    return
+
+}
