7e942d21f8851cc96a60d74b0257ab435d3b176c
Joyce Cerqueira da Silva <joyce.cerqueiras@gmail.com>
2023-03-22 23:56:06 -0300

    Alteração da estilização do programa
    Co-Authored-By: hussein1997323 <127331707+hussein1997323@users.noreply.github.com>

--- script.js
+++ script.js
@@ -23,5 +23,26 @@ agenda.addEventListener("click", fnNewPageThree)
 function fnNewPageThree(){
     alert ("ESTÁ FUNCIONANDO 3")
     return
-
+   
 }
+
+function mascara(i,t){
+
+    var v = i.value;
+ 
+    if(isNaN(v[v.length-1])){
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+ 
+    if(t == "cpf"){
+       i.setAttribute("maxlength", "14");
+       if (v.length == 3 || v.length == 7) i.value += ".";
+       if (v.length == 11) i.value += "-";
+    }
+    
+ else{
+          i.setAttribute("maxlength", "9");
+          if (v.length == 4) i.value += "-";
+       }
+ }
