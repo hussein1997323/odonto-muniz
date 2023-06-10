8de335d257d7b75275b5f0919950d4d93c24f9e3
hussein1997323 <127331707+hussein1997323@users.noreply.github.com>
2023-06-10 16:04:23 -0300

    test.php.bd

--- config.php
+++ config.php
@@ -1,26 +1,16 @@
 <?php
 $host = "localhost";
-$db_user = "root";
+$dbusername = "root";
 $db_password = "";
-$db_name = "usuario_senha";
+$db_name = "cadastro";
 
-$connect = mysqli_connect($host, $db_user, $db_password, $db_name );
 
-function login($connect){
-    if(isset($_POST['entra']) AND !empty($_POST['email'] and !$_POST['password'] )){
+$conexao = new mysqli($host, $dbusername, $db_password, $db_name );
 
-        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-        $senha = sha1($_POST['password']);
-
-    $query = "SELECT * FROM usuarios WHERE EMAIL = '$email' and senha ='$senha' ";
-    $executar = mysqli_query($connect, $query );
-    $return = mysqli_fetch_assoc($executar);
-if(!empty($return ['email'])){
- echo  $return ['email'];
-}else {
-    echo "usuário ou senha não encontrado!";}
-
-    echo $return ['email'];
-
-    }
+if($conexao-> connect_errno){
+    echo "erro";
 }
+else{
+    echo"conexão com sucesso";
+}
+?>
